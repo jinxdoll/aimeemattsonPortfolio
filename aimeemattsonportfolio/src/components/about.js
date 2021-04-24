ff27cdd0c37675b6dfe1f10d71ad3fde9f4d8ceb
amatt <amattson84@gmail.com>
2021-04-23 22:36:02 -0700

    Div section that will render a string paragraph witha short introduction.

--- aimeemattsonportfolio/src/components/about.js
+++ aimeemattsonportfolio/src/components/about.js
@@ -1,4 +1,40 @@
 import React, { Component } from 'react';
 export default class About extends Component {
-    render() {
-      return (
+  render() {
+    return (
+      <div>
+        <section className='colorlib-about' data-section='about'>
+          <div className='colorlib-narrow-content'>
+            <div className='row'>
+              <div className='col-md-12'>
+                <div
+                  className='row row-bottom-padded-sm animate-box'
+                  data-animate-effect='fadeInLeft'
+                >
+                  <div className='col-md-12'>
+                    <div className='about-desc'>
+                      <span className='heading-meta'>About Aimee</span>
+                      <h2 className='colorlib-heading'>Who Am I?</h2>
+                      <p>
+                        I am a student at the University of Oregon ((Go Ducks!))
+                        with Computer Coding and Software Development as my
+                        major.I love exploring new technologies, learning new
+                        developer languages and finding ways to expand my
+                        skills!
+                        <p>
+                          
+                          When I'm not sitting at the computer, you can find me
+                          enjoying the outdoors and takin
+                        </p>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
