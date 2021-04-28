5139f2fc516d4cdcf0bdf3aa31509bcc1fe1c066
amatt <amattson84@gmail.com>
2021-04-28 15:39:29 -0700

    Delete unneeded images.

--- aimeemattsonportfolio/src/components/sidebar.js
+++ aimeemattsonportfolio/src/components/sidebar.js
@@ -8,7 +8,7 @@ export default class Sidebar extends Component {
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-              {/* <div className="author-img" style={{backgroundImage: 'url(images/about.jpg)'}} /> */}
+              <div className="author-img" style={{backgroundImage: 'url(images/about.jpg)'}} />
               <h1 id="colorlib-logo"><a href="index.html">Aimee Mattson</a></h1>
               <span className="email"><i className="icon-mail"></i> amattson84@gmail.com</span>
             </div>
@@ -17,8 +17,8 @@ export default class Sidebar extends Component {
                 <ul>
                   <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
                   <li><a href="#about" data-nav-section="about">About</a></li>
-                  {/*<li><a href="#" data-nav-section="projects">Projects</a></li>
-                  <li><a href="#" data-nav-section="blog">Blog</a></li>*/}
+                  <li><a href="#projects" data-nav-section="projects">Projects</a></li>
+                  {/* <li><a href="#" data-nav-section="blog">Blog</a></li> */}
                   <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
                 </ul>
               </div>
